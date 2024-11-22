575f7b27d37ca041a6796c5183fd2d91761bb6c4
Raditya Maulana <radityamaulana27@gmail.com>
2024-11-22 14:39:13 +0700

    integrate and set landing page

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,29 +1,35 @@
-import './App.css';
 import './background.css';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+function LandingPage() {
+  const navigate = useNavigate();
 
-function App() {
   useEffect(() => {
     const svgElements = document.querySelectorAll('.svg-to-animate');
     svgElements.forEach((el) => el.classList.add('animate'));
-  }, []); // Efek hanya terjadi saat komponen dimuat pertama kali
+  }, []); 
+
+  const handleStart = (e) => {
+    e.preventDefault();
+    console.log("Button clicked"); // Debugging line
+    navigate('/instructions');
+  };
 
   return (
-      <div className="background">
-          <img className="wave-bg-bottom-left" src="/SVG/WaveBackground.svg" alt ="WaveBg" />
-          <img className="wave-bg-top-right" height={1000} src="/SVG/WaveBackground2.svg" alt ="WaveBg2" />
-          <img className="svg-left-top" height={650} src="/SVG/buletbulet.svg" alt="SvgBuletBulet" />
-          <img className="svg-bottom-right" height={650} src="SVG/buletbulet2.svg" alt="SvgBuletBulet2" />
-          <img className="mulai-survey" height={400} src="SVG/tulisanmulaisurvey.svg" alt="MulaiSurvey" />
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#F69090' }}>
-          <button className="custom-button">
-            Mulai 
-            </button>
-        </div>
+    <div className="background">
+      <img className="wave-bg-bottom-left" src="/SVG/WaveBackground.svg" alt="WaveBg" />
+      <img className="wave-bg-top-right" height={1000} src="/SVG/WaveBackground2.svg" alt="WaveBg2" />
+      <img className="svg-left-top" height={500} src="/SVG/buletbulet.svg" alt="SvgBuletBulet" />
+      <img className="svg-bottom-right" height={450} src="SVG/buletbulet2.svg" alt="SvgBuletBulet2" />
+      <img className="mulai-survey" height={300} src="SVG/tulisanmulaisurvey.svg" alt="MulaiSurvey" />
+      <div className="button-container">
+        <button className="custom-button" onClick={handleStart}>
+          Mulai
+        </button>
+      </div>
     </div>
   );
 }
 
-
-export default App;
+export default LandingPage;
