6b83f11e85a70820a0920d684682546a4b327ced
unknown <khairumayzal@gmail.com>
2024-11-22 12:52:06 +0700

    Ralat #3 landingpage

--- src/landingpage.js
+++ src/landingpage.js
@@ -12,7 +12,7 @@ function App() {
   return (
       <div className="background">
           <img className="wave-bg-bottom-left" src="/SVG/WaveBackground.svg" alt ="WaveBg" />
-          <img className="wave-bg-top-right" height={1000} src="/SVG/WaveBacground2.svg" alt ="WaveBg2" />
+          <img className="wave-bg-top-right" height={1000} src="/SVG/WaveBackground2.svg" alt ="WaveBg2" />
           <img className="svg-left-top" height={650} src="/SVG/buletbulet.svg" alt="SvgBuletBulet" />
           <img className="svg-bottom-right" height={650} src="SVG/buletbulet2.svg" alt="SvgBuletBulet2" />
           <img className="mulai-survey" height={400} src="SVG/tulisanmulaisurvey.svg" alt="MulaiSurvey" />
