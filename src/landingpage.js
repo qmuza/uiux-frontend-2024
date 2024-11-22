a44528c63f09459964d9e2cc5da96d0787a36683
unknown <khairumayzal@gmail.com>
2024-11-22 11:31:47 +0700

    Ralat #2 landingpage.js

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,11 +1,7 @@
 import './App.css';
 import './background.css';
 import { useEffect } from 'react';
-import SvgBuletBulet from './SVG/buletbulet.svg';
-import SvgBuletBulet2 from './SVG/buletbulet2.svg';
-import MulaiSurvey from './SVG/tulisanmulaisurvey.svg';
-import WaveBg from './SVG/WaveBackground.svg';
-import WaveBg2 from './SVG/WaveBackground2.svg';
+
 
 function App() {
   useEffect(() => {
@@ -15,11 +11,11 @@ function App() {
 
   return (
       <div className="background">
-          <img className="wave-bg-bottom-left" src={WaveBg} alt ="WaveBg" />
-          <img className="wave-bg-top-right" height={1000} src={WaveBg2} alt ="WaveBg2" />
-          <img className="svg-left-top" height={650} src={SvgBuletBulet} alt="SvgBuletBulet" />
-          <img className="svg-bottom-right" height={650} src={SvgBuletBulet2} alt="SvgBuletBulet2" />
-          <img className="mulai-survey" height={400} src={MulaiSurvey} alt="MulaiSurvey" />
+          <img className="wave-bg-bottom-left" src="/SVG/WaveBackground.svg" alt ="WaveBg" />
+          <img className="wave-bg-top-right" height={1000} src="/SVG/WaveBacground2.svg" alt ="WaveBg2" />
+          <img className="svg-left-top" height={650} src="/SVG/buletbulet.svg" alt="SvgBuletBulet" />
+          <img className="svg-bottom-right" height={650} src="SVG/buletbulet2.svg" alt="SvgBuletBulet2" />
+          <img className="mulai-survey" height={400} src="SVG/tulisanmulaisurvey.svg" alt="MulaiSurvey" />
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#F69090' }}>
           <button className="custom-button">
             Mulai 
