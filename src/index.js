fc9e66420d48bcee75188abbca04147004ccee05
abdullah faqih muzakki <abdullahfaqihmuzakki@gmail.com>
2024-11-24 22:03:37 +0700

    no longer necessary

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
 import InstructionPage from './instructionPage';
-import TestPage from './testprog';
 import MentalHealthSurvey from './MentalHealthSurvey';
 import './index.css';
 
@@ -14,7 +13,6 @@ root.render(
       <Routes>
         <Route path="/" element={<LandingPage />} />
           <Route path="/instructions" element={<InstructionPage />} />
-          <Route path="/test" element={<TestPage />} />
           <Route path="/survey" element={<MentalHealthSurvey />} />
       </Routes>
     </Router>
