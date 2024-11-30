9b320dfe189c10a3e32ac50de5b8feddd2132581
Raditya Maulana <radityamaulana27@gmail.com>
2024-11-30 19:22:24 +0700

    revisi button

--- src/instructionPage.js
+++ src/instructionPage.js
@@ -2,23 +2,6 @@ import React from 'react';
 import './instructionPage.css';
 import { useNavigate } from 'react-router-dom';
 
-const ChevronDown = () => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    width="24" 
-    height="24" 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    stroke="currentColor" 
-    strokeWidth="2" 
-    strokeLinecap="round" 
-    strokeLinejoin="round"
-    className="scroll-icon"
-  >
-    <path d="M6 9l6 6 6-6"/>
-  </svg>
-);
-
 const InstructionPage = () => {
   const navigate = useNavigate();
 
@@ -44,9 +27,9 @@ const InstructionPage = () => {
             </div>
 
             <div className="scroll-prompt">
-              <p>Klik panah di bawah untuk memulai</p>
-              <button className="down-button" onClick={() => navigate('/survey')}>
-                <ChevronDown />
+              <p>Klik tombol di bawah untuk memulai</p>
+              <button className="start-button" onClick={() => navigate('/survey')}>
+                <p>Mulai</p>
               </button>
             </div>
           </div>
